6305c0bee15585f38cbbe96e5f3be82c8155509a
Jacob Rienstra <jacobr@fb.com>
2019-09-06 01:10:12 -0400

    support url, update bower and package files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,65 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     concat: {
       options: {
-        separator: ';'
+        separator: ";"
       },
       dist: {
-        src: ['bower_components/Blob.js/Blob.js', 'bower_components/FileSaver/FileSaver.js', 'ics.js'],
-        dest: 'ics.deps.min.js'
+        src: [
+          "bower_components/Blob.js/Blob.js",
+          "bower_components/FileSaver/dist/FileSaver.js",
+          "ics.js"
+        ],
+        dest: "ics.deps.js"
       }
     },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today() %> */\n'
-      },
-      dist: {
+    terser: {
+      options: {},
+      main: {
         files: {
-          'ics.min.js': ['ics.js'],
-          'ics.deps.min.js': ['ics.deps.min.js'] 
+          "ics.min.js": ["ics.js"],
+          "ics.deps.min.js": ["ics.deps.js"]
         }
       }
     },
     mocha: {
-        all: {
-            src: ['test/index.html'],
-            options: {
-                run: true,
-                log: true
-                // urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
-            }
+      all: {
+        src: ["test/index.html"],
+        options: {
+          run: true,
+          log: true,
+          urls: [
+            "http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html"
+          ]
         }
+      }
     },
     jshint: {
-      files: ['Gruntfile.js', 'ics.js', 'test/spec/test.js'],
+      files: ["Gruntfile.js", "ics.js", "test/spec/test.js"],
       options: {
         // options here to override JSHint defaults
         globals: {
           jQuery: true,
           console: true,
           module: true,
-          document: true,
+          document: true
         },
         laxcomma: true
       }
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'mocha']
+      files: ["<%= jshint.files %>"],
+      tasks: ["jshint", "mocha"]
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-terser");
 
-  grunt.registerTask('test', ['jshint', 'mocha']);
+  grunt.registerTask("test", ["jshint", "mocha"]);
 
-  grunt.registerTask('default', ['jshint', 'mocha', 'concat', 'uglify']);
-
+  grunt.registerTask("default", ["jshint", "concat", "terser"]);
 };
